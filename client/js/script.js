edb50dfda06626cc906b6e9d007d4de32147772b
Bogdan Ceu <ceubogdan@yahoo.com>
2018-03-30 13:39:24 +0200

    inserer le html dans le dom

--- client/js/script.js
+++ client/js/script.js
@@ -4,4 +4,5 @@ fetch('http://localhost:3000/posts')
   .then(res => res.json())
   .then(posts => {
     const postsElements = document.getElementById('posts')
-  }
+    postsElements.innerHTML = posts.map(newPosts).join('')
+})
