d8e7da02714f683d1360063c12d7fe7f431638a0
Bogdan Ceu <ceubogdan@yahoo.com>
2018-03-30 14:06:01 +0200

    correction erreurs lint

--- client/js/script.js
+++ client/js/script.js
@@ -1,8 +1,8 @@
 import {newPosts} from '../modules/posts.js'
 
-fetch('http://localhost:3000/posts')
+window.fetch('http://localhost:3000/posts')
   .then(res => res.json())
   .then(posts => {
     const postsElements = document.getElementById('posts')
     postsElements.innerHTML = posts.map(newPosts).join('')
-})
+  })
