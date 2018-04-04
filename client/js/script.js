69bbefec32d0cf4d8c4a150062d82865f100dfe3
gaelleF <gaelle.fougere@outlook.fr>
2018-04-04 10:59:01 +0200

    ajout addEventlistener sur bouton top

--- client/js/script.js
+++ client/js/script.js
@@ -6,3 +6,15 @@ window.fetch('http://localhost:3000/posts')
     const postsElements = document.getElementById('posts')
     postsElements.innerHTML = posts.map(newPosts).join('')
   })
+
+// au clic sur topSort
+const topSortButton = document.getElementById('top_sort')
+
+topSortButton.addEventListener('click', (e) => {
+  window.fetch('http://localhost:3000/posts')
+    .then(res => res.json())
+    .then(posts => {
+      const postsElements = document.getElementById('posts')
+      postsElements.innerHTML = posts.map(newPosts).join('')
+    })
+})
