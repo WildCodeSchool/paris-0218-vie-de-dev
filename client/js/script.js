2eaeb699b4d5dd216c6154161b87a6c3a7807d59
gaelleF <gaelle.fougere@outlook.fr>
2018-04-04 11:07:03 +0200

    filtre et tri les posts yes

--- client/js/script.js
+++ client/js/script.js
@@ -14,7 +14,13 @@ topSortButton.addEventListener('click', (e) => {
   window.fetch('http://localhost:3000/posts')
     .then(res => res.json())
     .then(posts => {
+      let postSort = posts.filter((post) => {
+        return post.yesVotes.length > 0
+      })
+      postSort = postSort.sort((a, b) => {
+        return (b.yesVotes.length - a.yesVotes.length)
+      })
       const postsElements = document.getElementById('posts')
-      postsElements.innerHTML = posts.map(newPosts).join('')
+      postsElements.innerHTML = postSort.map(newPosts).join('')
     })
 })
