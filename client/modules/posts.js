b71b31a01e2d2bc3191c74ba8258704ad5826296
Bogdan Ceu <ceubogdan@yahoo.com>
2018-04-05 09:53:26 +0200

    ajout de bouton Yes

--- client/modules/posts.js
+++ client/modules/posts.js
@@ -1,9 +1,11 @@
 export const newPosts = (post) => {
   return `
     <div class='divposts'>
-    <h4>VDD</h4>
-    <p>Home/user-VDD:</p>
-    <p>${post.content}</p>
+      <h4>VDD</h4>
+      <p>Home/user-VDD:</p>
+      <p>${post.content}</p>
     </div>
+    <div class='boutons'>
+      <button>YES</button>
     `
 }
