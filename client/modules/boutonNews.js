59ab83fdccbcc08478601ef49a66eec0148a224f
Bogdan Ceu <ceubogdan@yahoo.com>
2018-04-10 17:14:34 +0200

    creaton module bouton news et random

---
+++ client/modules/boutonNews.js
@@ -0,0 +1,10 @@
+ export const boutonNews = () => {
+   const newsBtn = document.getElementById('news_sort')
+   newsBtn.addEventListener('click', () => {
+     window.fetch('http://localhost:3000/posts')
+      .then(res => res.json())
+      .then(posts => {
+        const buttonElements = document.getElementById('posts')
+        buttonElements.innerHTML = posts.map(newPosts).reverse().join('')
+      })
+    })
