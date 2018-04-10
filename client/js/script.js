70941389cbb98e8a4fa9ee3024f27ab8b51c413d
Ilmio <facchin.carlos@yahoo.fr>
2018-04-10 14:35:19 +0200

    adding the script for the native script of the news page and boutton news

--- client/js/script.js
+++ client/js/script.js
@@ -5,4 +5,13 @@ window.fetch('http://localhost:3000/posts')
   .then(posts => {
     const postsElements = document.getElementById('posts')
     postsElements.innerHTML = posts.map(newPosts).join('')
+    // console.log(posts.map(newPosts).reverse().join(''))
+  })
+  newsButton.addEventListener('click' , () =>{
+    window.fetch('http://localhost:3000/posts')
+    .then(res => res.json())
+    .then(posts => {
+      const buttonElements = document.getElementById('posts')
+      buttonElements.innerHTML = posts.map(newPosts).reverse().join('')
+    })
   })
