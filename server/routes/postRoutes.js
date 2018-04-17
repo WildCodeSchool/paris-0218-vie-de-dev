327ced33b1870fe10a1f62f35ec8c943264939ad
gaelleF <gaelle.fougere@outlook.fr>
2018-04-17 21:04:42 +0200

    ajout routes pour  formulaire soumettre

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const bodyParser = require('body-parser')
 const fs = require('fs')
 const path = require('path')
+const util = require('util')
+
+const writeFile = util.promisify(fs.writeFile)
 
 router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({extended: true}))
@@ -35,10 +38,28 @@ router.post('/vote/:typeVote', (req, res, next) => {
   })
 })
 
+
+
 router.post('/soumettre',(req,res,next) => {
-  console.log('post/soumettre'+req.body.idUsers)
+  console.log('post/soumettre'+req.body)
+  //nom fichier aleatoire : test sur nom ?
+  const id = Math.random().toString(36).slice(2).padEnd(11, '0').slice(0, 5)
+  const filename = `${id}.json`
+  const filepath = path.join(__dirname, '../../mocks/post/', filename)
 
-
-})
+  const contentPost = {
+    id: id,
+    userId: req.body.userId,
+    content: req.body.content,
+    badVotes: [],
+    yesVotes: [],
+    saltyVotes: [],
+    createdAt: Date.now()
+  }
+  //write (promisify)
+  writeFile(filepath, JSON.stringify(contentPost),'utf-8')
+    .then(() => res.json('OK'))
+    .catch(err => next())
+  })
 
 module.exports = router
