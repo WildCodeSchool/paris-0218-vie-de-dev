141de6bf508497a10a3a5379b09a2b3900f019f0
gaelleF <gaelle.fougere@outlook.fr>
2018-04-19 18:43:43 +0200

    correction erreur lint

--- client/js/script_form.js
+++ client/js/script_form.js
@@ -1,10 +1,3 @@
 document.getElementById('bouton').addEventListener('submit', event => {
-	event.preventDefault()
-	const newPost = document.getElementById('formulaire').value
-
-	fetch('http://localhost:3000/post/soumettre', {
-		method: 'post',
-		userId: 8,
-		content: 'blague de dev'
-	}).then(res => console.log(res.status))
+  event.preventDefault()
 })
