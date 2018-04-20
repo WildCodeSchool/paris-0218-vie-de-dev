a7d73425e2046077f744b26a0815e615277072eb
gaelleF <gaelle.fougere@outlook.fr>
2018-04-20 14:24:37 +0200

    news par date

--- client/modules/boutonNews.js
+++ client/modules/boutonNews.js
@@ -2,11 +2,16 @@ import {newPosts} from '../modules/posts.js'
 export const boutonNews = () => {
   const newsBtn = document.getElementById('news_sort')
   newsBtn.addEventListener('click', () => {
+
     window.fetch('http://localhost:3000/posts')
       .then(res => res.json())
       .then(posts => {
+      	  	console.log(posts)
+        let postSort = posts.sort((a, b) => {
+          return (b.createdAt - a.createdAt)
+        })
         const buttonElements = document.getElementById('posts')
-        buttonElements.innerHTML = posts.map(newPosts).reverse().join('')
+        buttonElements.innerHTML = posts.map(newPosts).join('')
       })
   })
 }
