3e198658529a16594c782cfbd65d14f42640e203
pierrep <pierreporetti@gmail.com>
2018-04-20 11:46:52 +0200

    mise à jour script_form.js + correction travis

--- client/js/script_form.js
+++ client/js/script_form.js
@@ -1,15 +1,16 @@
+/* global URLSearchParams */
 document.getElementById('add_post').addEventListener('submit', event => {
   event.preventDefault()
 
   const content = document.getElementById('new_post').value
   console.log(content)
-
   window.fetch('http://localhost:3000/post/soumettre', {
     method: 'post',
-	body: new URLSearchParams({
-	  userID: 8,
-	  content: content
-	})
+
+    body: new URLSearchParams({
+      userID: 8,
+      content: content
+    })
   })
-  .then(res => console.log(res.status))
+    .then(res => console.log(res.status))
 })
