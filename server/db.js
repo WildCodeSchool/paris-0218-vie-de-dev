ecdba3a1ccae158e2e15a411340ee56306194f46
Bogdan Ceu <ceubogdan@yahoo.com>
2018-04-25 09:25:47 +0200

    creation function getPosts

--- server/db.js
+++ server/db.js
@@ -1,5 +1,14 @@
+const mysql = require('mysql2/promise')
+const co = mysql.createConnection({
+  host: 'localHost'
+  user: 'root'
+  database: 'VDD'
+})
 const getPosts = () => {
-  
+  return co.then(connection => {
+    return connection.execute('SELECT * FROM post')
+      .then((result) => result[0])
+  })
 }
 
 
