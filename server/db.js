db89230f4cc1460aafb07216b8b3bf8e263c6933
gaelleF <gaelle.fougere@outlook.fr>
2018-04-25 14:03:02 +0200

    ajout fonction requete SQL vote

--- server/db.js
+++ server/db.js
@@ -23,6 +23,20 @@ const addPost = (params) =>
   .then(result => console.log('result:', result))
   .catch(console.error) */
 
+const addVote = (params, table) =>
+  exec(`INSERT INTO ${table} (userId, postId) VALUES (?, ?)`,
+    [params.user, params.idPost])
+
+const selectVote = (params, table) => 
+  exec(`SELECT * FROM ${table} WHERE userId = ? AND postId = ?`,
+    [params.user, params.idPost])
+
+/*selectVote({user: 2, idPost:1},'yesVotes')
+    .then(result => console.log('result:', result))
+    .catch(console.error)*/
+
 module.exports = {
-  addPost
+  addPost,
+  addVote,
+  selectVote
 }
