eff7690b391d39ffe57f6d72f35db23b8dafc684
gaelleF <gaelle.fougere@outlook.fr>
2018-04-27 12:12:43 +0200

    affichage du post dans la page comments :)

--- client/js/script_comment.js
+++ client/js/script_comment.js
@@ -1,8 +1,18 @@
 import { createCommentElement } from '../modules/createComment.js'
-
+import {newPosts} from '../modules/posts.js'
 
 const params = new URLSearchParams(window.location.search)
 const id = params.get('id')
+
+fetch(`http://localhost:3000/postComment/${id}`)
+  .then(res => res.json())
+  .then(post => {
+    const postElement = document.getElementById('postDetail')
+  	postElement.innerHTML = post.map(newPosts).join('')
+  	console.log('post res :', post)
+  	
+  })
+
 
 fetch(`http://localhost:3000/comments/${id}`)
   .then(res => res.json())
