faf576edebaf03a082630266dffbcba1a26b79ce
gaelleF <gaelle.fougere@outlook.fr>
2018-04-27 18:56:19 +0200

    ajout addListener sur submit pour ajouter com dans db

--- client/js/script_comment.js
+++ client/js/script_comment.js
@@ -1,5 +1,9 @@
 import { createCommentElement } from '../modules/createComment.js'
 import {newPosts} from '../modules/posts.js'
+import {createVoteYes} from '../modules/createVote.js'
+import {createVoteSalty} from '../modules/createVoteSalty.js'
+import {createVoteBad} from '../modules/createVoteBad.js'
+import {createSearch} from '../modules/createSearch.js'
 
 /* global URLSearchParams */
 
@@ -12,6 +16,11 @@ window.fetch(`http://localhost:3000/postComment/${id}`)
     const postElement = document.getElementById('post_detail')
     postElement.innerHTML = post.map(newPosts).join('')
     console.log('post res :', post)
+    createVoteYes()
+    createVoteSalty()
+    createVoteBad()
+    createSearch('search_btn')
+    createSearch('search_btn_tel')
   })
 
 window.fetch(`http://localhost:3000/comments/${id}`)
@@ -21,3 +30,21 @@ window.fetch(`http://localhost:3000/comments/${id}`)
     commentElement.innerHTML = comments.map(createCommentElement).join('')
     console.log('comment res :', comments)
   })
+
+ /* global URLSearchParams */
+document.getElementById('add_com').addEventListener('submit', event => {
+  event.preventDefault()
+
+  const content = document.getElementById('new_com').value
+  console.log(content)
+  window.fetch('http://localhost:3000/addComments', {
+    method: 'post',
+    body: new URLSearchParams({
+      userId: 4,
+      postId: id,
+      content: content
+    })
+  })
+    .then(res => console.log(res.status))
+})
+
