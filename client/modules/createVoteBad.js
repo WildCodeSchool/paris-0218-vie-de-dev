8f18ae9c3d9bb442b39df21be0055a6713c2f7d8
Bogdan Ceu <ceubogdan@yahoo.com>
2018-05-02 17:14:54 +0200

    creationbadvotes only if connected

--- client/modules/createVoteBad.js
+++ client/modules/createVoteBad.js
@@ -1,26 +1,30 @@
 export const createVoteBad = () => {
-  /* global URLSearchParams */
-  let userConnect = 3 // utilisateur connecté fictif
-  const badVoteClass = document.getElementsByClassName('badVote')
-
-  for (let butt of badVoteClass) {
-    butt.addEventListener('click', (e) => {
-      // recupération id post
-      console.log('idPost : ' + e.target.id)
-      let idPost = (e.target.id).split('_')[1]
-      // envoi au serveur 'http://localhost:3000/postVote/:voteType'
-      window.fetch('http://localhost:3000/post/vote/badVotes', {
-        method: 'post',
-        body: new URLSearchParams(
-          { id: idPost,
-            user: userConnect })
-      })
-        .then(res => res.json())
-        .then(res => {
-          console.log(res) // reponse serveur : renvoi les données du mock modifié
-          const badCompteur = document.getElementById(`badCompt_${idPost}`)
-          badCompteur.textContent = res[0].nbVotes
-        })
+  window.fetch('http://localhost:3000/', { 'credentials': 'include' })
+    .then(user => user.json())
+    .then(user => {
+      if (user.id) {
+        let userConnect = user.id // utilisateur connecté fictif
+        const badVoteClass = document.getElementsByClassName('badVote')
+        for (let butt of badVoteClass) {
+          butt.addEventListener('click', (e) => {
+            // recupération id post
+            console.log('idPost : ' + e.target.id)
+            let idPost = (e.target.id).split('_')[1]
+            // envoi au serveur 'http://localhost:3000/postVote/:voteType'
+            window.fetch('http://localhost:3000/post/vote/badVotes', {
+              method: 'post',
+              body: new URLSearchParams(
+                { id: idPost,
+                  user: userConnect })
+            })
+              .then(res => res.json())
+              .then(res => {
+                const badCompteur = document.getElementById(`badCompt_${idPost}`)
+                badCompteur.textContent = res[0].nbVotes
+              })
+          })
+        }
+      }
     })
-  }
 }
+  
