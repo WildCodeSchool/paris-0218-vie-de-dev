9e08b46703688355e85d54b07f1f737a645fb655
Bogdan Ceu <ceubogdan@yahoo.com>
2018-05-02 17:10:50 +0200

    createvoteYes only if connected

--- client/modules/createVote.js
+++ client/modules/createVote.js
@@ -1,25 +1,30 @@
 export const createVoteYes = () => {
   /* global URLSearchParams */
-  let userConnect = 1 // utilisateur connecté fictif
-  const yesVoteClass = document.getElementsByClassName('yesVote')
-
-  for (let butt of yesVoteClass) {
-    butt.addEventListener('click', (e) => {
-      // recupération id post
-      console.log('idPost : ' + e.target.id)
-      let idPost = (e.target.id).split('_')[1]
-      // envoi au serveur 'http://localhost:3000/postVote/:voteType'
-      window.fetch('http://localhost:3000/post/vote/yesVotes', {
-        method: 'post',
-        body: new URLSearchParams(
-          { id: idPost,
-            user: userConnect })
-      })
-        .then(res => res.json())
-        .then(res => {
-          const yesCompteur = document.getElementById(`yesCompt_${idPost}`)
-          yesCompteur.textContent = res[0].nbVotes
-        })
+  window.fetch('http://localhost:3000/', { 'credentials': 'include' })
+    .then(user => user.json())
+    .then(user => {
+      if (user.id) {
+        let userConnect = user.id // utilisateur connecté fictif
+        const yesVoteClass = document.getElementsByClassName('yesVote')
+        for (let butt of yesVoteClass) {
+          butt.addEventListener('click', (e) => {
+            // recupération id post
+            console.log('idPost : ' + e.target.id)
+            let idPost = (e.target.id).split('_')[1]
+            // envoi au serveur 'http://localhost:3000/postVote/:voteType'
+            window.fetch('http://localhost:3000/post/vote/yesVotes', {
+              method: 'post',
+              body: new URLSearchParams(
+                { id: idPost,
+                  user: userConnect })
+            })
+              .then(res => res.json())
+              .then(res => {
+                const yesCompteur = document.getElementById(`yesCompt_${idPost}`)
+                yesCompteur.textContent = res[0].nbVotes
+              })
+          })
+        }
+      }
     })
-  }
 }
