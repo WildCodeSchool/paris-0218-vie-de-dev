bb1c1f358403e99f68a0ee0f8ac0a0c2909c903b
Bogdan Ceu <ceubogdan@yahoo.com>
2018-05-02 17:58:55 +0200

    regler le droit de poster si tu n'est pas connecté

--- client/js/script_form.js
+++ client/js/script_form.js
@@ -7,15 +7,22 @@ document.getElementById('add_post').addEventListener('submit', event => {
   window.fetch('http://localhost:3000/', {credentials: 'include'})
     .then(res => res.json())
     .then(res => {
-      console.log(res)
-      window.fetch('http://localhost:3000/post/soumettre', {
-        method: 'post',
+      if (res.id) {
+        console.log(res)
+        window.fetch('http://localhost:3000/post/soumettre', {
+          method: 'post',
 
-        body: new URLSearchParams({
-          userId: res.id,
-          content: content
+          body: new URLSearchParams({
+            userId: res.id,
+            content: content
+          })
         })
-      })
-        .then(res => window.location.replace('/'))
+          .then(res => window.location.replace('/'))
+      } else {
+        document.getElementById('add_post').innerHTML = `
+        <textarea id="new_post" name="new_post" placeholder="Tu n'as pas le droit de poster si tu n'est pas connecté !"></textarea><br>
+        <button id="bouton" type="submit">VALIDER</button>
+      `
+      }
     })
 })
