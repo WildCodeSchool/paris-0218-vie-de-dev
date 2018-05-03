b15b0fa5eaf6ccaa041e38d16a6347dba720bfbb
Clement Denis <le.mikmac@gmail.com>
2018-05-03 11:20:31 +0200

    display proper error to client

--- client/modules/addUser.js
+++ client/modules/addUser.js
@@ -3,21 +3,24 @@ export const addUser = () => {
     event.preventDefault()
     const messageElement2 = document.getElementById('message2')
     const URLSearchParams = window.URLSearchParams
-    const name = document.getElementById('username_sub').value
-    const email = document.getElementById('email_sub').value
-    const password = document.getElementById('password_sub').value
+    const name = document.getElementById('username_sub')
+    const email = document.getElementById('email_sub')
+    const password = document.getElementById('password_sub')
+    const fields = { name, email, password }
+
     window.fetch('http://localhost:3000/addUser', {
       method: 'post',
-
       body: new URLSearchParams({
-        name: name,
-        email: email,
-        password: password
+        name: name.value,
+        email: email.value,
+        password: password.value
       })
     })
       .then(res => res.json())
       .then(res => {
         if (res.error) {
+          const errorField = fields[res.data.key]
+          errorField.select()
           messageElement2.innerHTML = res.error || ''
         } else {
           window.location.replace('/')
